170f962e9b920fcd3937af108001262aa2b7b374
Greg Cunningham <greg@curioustech.net>
2025-05-07 18:29:16 -0400

    Added LD2450 reader

--- Waterbed3/Radar.h
+++ Waterbed3/Radar.h
@@ -5,6 +5,17 @@
 #include "eeMem.h"
 #include <ld2410.h> // from Library Manager in Arduino IDE
 
+#define LD2450
+#define LD2450_BUFFER_SIZE (10*30)
+
+struct RadarTarget
+{
+  int16_t x;           // X mm
+  int16_t y;           // Y mm
+  int16_t speed;       // cm/s
+  uint16_t resolution; // mm
+};
+
 class Radar
 {
 public:
@@ -16,11 +27,21 @@ public:
   bool m_bPresence;
   bool m_bLightOn;
   bool m_bInBed;
+  uint8_t nZone = 2;
+  uint8_t bits;
+  int16_t nDistance = 400; // start out of zones
+  uint16_t nEnergy;
 
 private:
+#ifdef LD2450
+  RadarTarget radTgt[3];
+  uint8_t circular_buffer[LD2450_BUFFER_SIZE];
+  uint16_t buffer_head = 0;
+  uint16_t buffer_tail = 0;
+#else
   ld2410 m_ld;
+#endif
   bool radarConnected = false;
-
 };
 
 extern Radar radar;
