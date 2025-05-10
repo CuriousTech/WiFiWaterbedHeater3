b92809e3358f1915c001d235ed30bcedffaec7e6
Greg Cunningham <greg@curioustech.net>
2025-05-10 12:32:22 -0400

    Added radar settings

--- Waterbed3/eeMem.h
+++ Waterbed3/eeMem.h
@@ -41,7 +41,7 @@ public:
 
   uint16_t size = EESIZE;            // if size changes, use defaults
   uint16_t sum = 0xAAAA;             // if sum is different from memory struct, write
-  uint16_t nWriteCnt;               // just a counter for later checking
+  uint16_t nWriteCnt;                // just a counter for later checking
   char     szSSID[24] = "";  // Enter you WiFi router SSID
   char     szSSIDPassword[24] = ""; // and password
   char     iotPassword[24] = "password"; // password for controlling dimmers and switches
@@ -50,14 +50,14 @@ public:
   uint8_t  hvacIP[4] = {192,168,31,46};
   uint16_t hvacPort = 80;
   uint8_t  brightLevel = 180; // brightness
-  uint16_t sleepTime = 60; // timer for sleep mode
+  uint16_t sleepTime = 60;   // timer for sleep mode
 
-  uint16_t vacaTemp = 700;     // vacation temp
+  uint16_t vacaTemp = 700;   // vacation temp
   uint8_t schedCnt[4] = {5,5,5,5};   // number of active scedules
   int8_t  tz = -5;
-  bool    bVaca = false;         // vacation enabled
-  bool    bAvg = true;          // average target between schedules
-  bool    bEco = false;          // eco mode
+  bool    bVaca = false;   // vacation enabled
+  bool    bAvg = true;    // average target between schedules
+  bool    bEco = false;  // eco mode
   bool    bCF = false;  // true = celcius
   uint16_t scheduleDays[4] = {77, 155, 171, 355}; // Spring, Summer, Fall, Winter
 
@@ -124,12 +124,13 @@ public:
   uint16_t ppkwh = 159; // $0.154 / KWH
   uint16_t rate = 50; // seconds
   uint16_t watts = 290; // Heating pad
-  uint32_t tSecsMon[12] = {1254411,1067144,916519,850686,122453,268488,302535,396531,501161,552347,427980,883172}; // total secwatt hours per month (copied from page)
+  uint32_t tSecsMon[12] = {1254411,1067144,916519,850686,122453,268488,302535,396531,501161,552347,427980,883172}; // total secwatt hours per month (chrome console)
   int16_t  tAdj[2] = {0,0};
   int16_t  pids[3] = {60*3,60*1, 5}; // Todo: real PID
-  int16_t  radarPts[4][2] = {-400,100, 400,100, 400,1500, -400,1500};
-
-  uint8_t  reserved[34];           // Note: To force an EEPROM update, just subtract 1 byte and flash again
+  int16_t  radarPts[4][2] = {{-1450,107}, {812,133}, {593,2258}, {-1575,1866}}; // bound box for zone 0
+  uint32_t blindBits[32]; // 32x32 editable blindspots
+  uint16_t bound = 6000; // boundary in mm
+  uint8_t  reserved[2];           // Note: To force an EEPROM update, just subtract 1 byte and flash again
   uint8_t  end;
 };
 
